9b80202703645278150ae3e3bf36b764f199f535
Carpmenter <nickcarpenter1017@gmail.com>
2020-11-07 17:21:44 -0600

    added email notification

--- NeweggScraper.py
+++ NeweggScraper.py
@@ -2,14 +2,28 @@ from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import smtplib, ssl
+from email.mime.text import MIMEText
 
-#driver = webdriver.Firefox()
+#python -m smtpd -c DebuggingServer -n localhost:1025
 
-#URL Setup
-graphicsCard = "rtx+2060"
+#variables and URL setup
+max_price = 2600
+min_price = 500
+graphicsCard = "RTX 3080"
 frontURL = 'https://www.newegg.com/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Description='
 backURL = '&N=-1&isNodeId=1'
-url = frontURL + graphicsCard + backURL
+url = frontURL + graphicsCard.replace(' ', '+') + backURL
+
+print(url)
+
+# email setup 64.233.184.108
+port = 587
+smtp_server = 'smtp.gmail.com'
+email = 'nica.dev2020@gmail.com'
+password = "Carp_1017"
+context = ssl.create_default_context()
+
 
 #gets page and parses html
 page = uReq(url)
@@ -19,14 +33,16 @@ page_soup = soup(page_html, "html.parser")
 
 #grab all product containers
 containers = page_soup.findAll("div", {"class":"item-container"})
+#print(containers)
 
-#search starts at index 4, 0-3 are ads
-for i in range(4, len(containers)):
+#loop through each container
+for i in range(0, len(containers)):
 	container = containers[i]
 
-	#grabs item title
+	#grab item title and url
 	titleContainer = container.findAll("a", {"class":"item-title"})
 	productName = titleContainer[0].text.strip()
+	itemURL = container.findAll('a', {'class':'item-img'})[0]['href']
 
 	#grab item price
 	priceContainer = container.findAll("li", {"class":"price-current"})
@@ -34,29 +50,46 @@ for i in range(4, len(containers)):
 		productPrice = priceContainer[0].text.strip().split()[0][1:].replace(',','')
 		productPrice = float(productPrice)
 	except IndexError:
-		productPrice = "N/A"
+		productPrice = 10000
 
+	print("-----------------------------------------------------------------\n%s" % productName)
 	print(productPrice)
 
     # Get status of item
 	buttonContainer = container.findAll("button", {"class":"btn"}) # resultSet
 	try:
 		buttonStatus = buttonContainer[0]['title']
+		if buttonStatus[:4] + buttonStatus[-7:] == "Add to cart":
+			buttonStatus = "Add to cart"
 	except IndexError:
 		buttonStatus = "Not Available"
 
-	#print("\'%s\'" % buttonStatus)
+	print("\'%s\'" % buttonStatus)
 
-	#grabs shipping price
-	shippingContainer = container.findAll("li", {"class":"price-ship"})
-	shippingCost = shippingContainer[0].text.strip()
+	# Create Email message
+	message = "Click this link to buy: %s" % (itemURL)
+	SUBJECT = graphicsCard + " IN STOCK!"
+	message = MIMEText(message)
+	message['Subject'] = SUBJECT
+	message['To'] = email
+	message['From'] = 'Newegg Bot'
 
-	#print(productName)
-	#print(productPrice)
-	#print(shippingCost)
+	# check if item is available
+	if (buttonStatus == 'View Details ' or buttonStatus == "Add to cart") and (productPrice < max_price and productPrice > min_price):
+		print("<><><><> BUY <><><><>")
 
-	# check for in stock and add to cart
-	# if buttonStatus == 'View Details ' and productPrice < 600:
-	# 	print("buy")
-	# else:
-	# 	print("Out of stock :( cant buy")
+		# login and send email message
+		try:
+			server = smtplib.SMTP(smtp_server, port)
+			server.ehlo() # Can be omitted
+			server.starttls(context=context)
+			server.ehlo() # Can be omitted
+			server.login(email, password)
+			server.sendmail(email, email, message.as_string())
+		except Exception as e:
+			print(e)
+		finally:
+			server.quit()
+
+	else:
+		print("Out of stock :(")
